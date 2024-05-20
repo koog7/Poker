c75fca0f966c604abead84e2fdfea73316172f1a
koog7 <k.ognev709@mail.com>
2024-05-20 11:08:32 +0600

    Уровень 3 , выполнен

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,47 @@
-import './App.css'
+import React, { useState } from 'react';
+import CardDeck from './lib/CardDeck';
+import Card from './lib/Card';
+import './App.css';
 
-function App() {
+const App: React.FC = () => {
 
-  return (
-    <>
+    const [deck, setDeck] = useState<CardDeck>(new CardDeck());
+    const [cards, setCards] = useState<Card[]>([]);
 
-    </>
-  )
-}
+    const dealCards = () => {
+        const newCards = deck.getCards(5);
+        console.log(newCards)
+        console.log(deck.cards.length);
+        setCards(newCards);
+        setDeck(deck);
+    };
 
-export default App
+    return (
+        <div>
+            <p>Количество карт: {deck.cards.length}</p>
+            <button onClick={dealCards}>Раздать карты</button>
+            {cards.length === 0 ? null : (
+                <div className="playingCards faceImages">
+                    {cards.map((card, index) => (
+                        <span key={index} className={`card rank-${card.rank.toLowerCase()} ${card.suit}`}>
+                            <span className="rank">{card.rank}</span>
+                            <span className="suit">{getSuitSymbol(card.suit)}</span>
+                        </span>
+                    ))}
+                </div>
+            )}
+        </div>
+    );
+};
+
+const getSuitSymbol = (suit: string) => {
+    const symbols: { [key: string]: string } = {
+        hearts: '♥',
+        diams: '♦',
+        clubs: '♣',
+        spades: '♠',
+    };
+    return symbols[suit];
+};
+
+export default App;
